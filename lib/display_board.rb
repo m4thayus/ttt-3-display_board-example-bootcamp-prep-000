10f0a9660acaf7d01d35ee1d21083987ae21d6dc
Matt Williams <matt.seongjun@gmail.com>
2019-03-23 13:01:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 def display_board
- 2.times do 
-  2.times do
-    3.times print " "
-    print "|"
+  2.times do 
+    2.times do
+      3.times print " "
+      print "|"
+    end
+    puts "   "
+    puts "-----------"
   end
-  puts "   "
-  puts "-----------"
- end
- puts "   |   |   "
+  puts "   |   |   "
